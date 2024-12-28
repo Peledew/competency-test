ef5bb8e37eba01fba4d89f9caf95db38d3dc8416
Luka-0 <93083090+Peledew@users.noreply.github.com>
2024-12-28 02:04:52 +0100

    Header finished, Hero section started.
    Heder is fully responsible, I just need to optimize code.
    In hero section slider iz added and possitioning is setted up for now.

--- js/script.js
+++ js/script.js
@@ -8,3 +8,24 @@ document.addEventListener('DOMContentLoaded', () => {
     navMenu.classList.toggle('active'); // Toggle active class to show/hide menu
   });
 });
+
+//Hero section
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slider img');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+function updateSlider() {
+  const slideWidth = slides[0].clientWidth;
+  document.querySelector('.slider').style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+}
+
+prevBtn.addEventListener('click', () => {
+  currentSlide = (currentSlide === 0) ? slides.length - 1 : currentSlide - 1;
+  updateSlider();
+});
+
+nextBtn.addEventListener('click', () => {
+  currentSlide = (currentSlide === slides.length - 1) ? 0 : currentSlide + 1;
+  updateSlider();
+});
